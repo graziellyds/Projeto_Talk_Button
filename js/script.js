26bcc5e3e617d066a8f81b97b4334425539424c3
Grazielly da Silva <graziellyds1996@gmail.com>
2024-11-24 22:32:17 -0300

    Projeto apresentado com funcionalidade pronto

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 function menuShow(){
+    /*Cria uma variavel*/
     let menuMobile = document.querySelector('.mobile-menu');
+    /* menu tá igual a display: none;*/
+
+    /*se ele clicar recebe a os atributos/caracterificas do open (display: block) */
     if (menuMobile.classList.contains('open')) {
+       /*remove para continuar fechado o open*/
         menuMobile.classList.remove('open');
         document.querySelector('.icon').src = "img/menu.svg";
     } else {
+        /*adiciona para continuar aberto o menu*/
         menuMobile.classList.add('open');
         document.querySelector('.icon').src = "img/close.svg";
     }
